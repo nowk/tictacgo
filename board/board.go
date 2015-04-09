39d2d4de054e4c5716e3f1b7b0a3e0b9e50de982
David Ko <davidko@Davids-Mac-Pro.home>
2015-04-08 21:11:57 -0400

    Create board struct

---
+++ board/board.go
@@ -0,0 +1,8 @@
+package board
+
+type Board struct {
+}
+
+func (b *Board) Display() string {
+  return "__|__|__\n__|__|__\n  |  |  "
+}
---
+++ board/board_test.go
@@ -0,0 +1,12 @@
+package board
+
+import "testing"
+
+func TestDisplay (t *testing.T) {
+  var board Board
+  boardDrawing := "__|__|__\n__|__|__\n  |  |  "
+
+  if board.Display() != boardDrawing {
+    t.Fail() 
+  }
+}
